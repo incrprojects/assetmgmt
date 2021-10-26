879d35698e1b4981761d7bbee102ec49b76cf1b5
incredibledevelopers <56466746+incredibledevelopers@users.noreply.github.com>
2021-10-26 11:58:08 +0530

    Database connection changed

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','asset_mgmt_db') or die(mysqli_error());
+$conn = mysqli_connect('us-cdbr-east-04.cleardb.com','b23bebf9cc8526','15cd97ea','heroku_9c3b7c41a04fbdb') or die(mysqli_error());
 
 ?>
